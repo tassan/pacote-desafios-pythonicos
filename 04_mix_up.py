119e5a0ae489e79dece0942bda3b1a60fd7a36a6
tassan <ftassan@outlook.com>
2021-03-17 00:18:12 -0300

    Teste com string de tamanho 1

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -42,3 +42,4 @@ if __name__ == '__main__':
     test(mix_up, ('dog', 'dinner'), 'dig donner')
     test(mix_up, ('gnash', 'sport'), 'spash gnort')
     test(mix_up, ('pezzy', 'firm'), 'fizzy perm')
+    test(mix_up, ('o', 'a'), 'a o')
