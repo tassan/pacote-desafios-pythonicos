bf758b9c10014ba17cdc22560af8878e5ed641ad
tassan <ftassan@outlook.com>
2021-03-17 00:10:02 -0300

    Solucao final para mix up

--- 04_mix_up.py
+++ 04_mix_up.py
@@ -14,10 +14,7 @@ Assuma que a e b tem tamanho 2 ou maior.
 
 def mix_up(a, b):
     # +++ SUA SOLUÇÃO +++
-    first_two_a = a[:2]
-    first_two_b = b[:2]
-
-    return a.replace(first_two_a, first_two_b) + ' ' + b.replace(first_two_b, first_two_a)
+    return ' '.join([a.replace(a[:2], b[:2]), b.replace(b[:2], a[:2])])
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
